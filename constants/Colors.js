8f8e27d7ac73722c22549ac8951ab179bd330655
AvaPodrazhansky <avapod@gmail.com>
2020-03-16 17:00:21 -0400

    Cleaned Up Line Chart Code

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,29 @@
 const tintColor = '#2f95dc';
 
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+  // Default Built In Colors
+    tintColor,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColor,
+    tabBar: '#fefefe',
+    errorBackground: 'red',
+    errorText: '#fff',
+    warningBackground: '#EAEB5E',
+    warningText: '#666804',
+    noticeBackground: tintColor,
+    noticeText: '#fff',
+
+  // Logo Colors
+    LIGHT_BACKGROUND: "#E6F3F9",
+    LIGHT_BLUE: "#038FDA",
+    DARK_BLUE: "#143D97",
+    LIGHT_ORANGE: "#F7B62D",
+    DARK_ORANGE: "#ED741D",
+    VERY_DARK_ORANGE: "#CB4E12",
+    DARK_BROWN: "#733B08",
+
+  // Grayscale
+    WHITE: "#FFFFFF",
+    DARK_GRAY: "#7B7B7B",
+    LIGHT_GRAY: "#cbcbcb",
 };

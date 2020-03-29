6f3c8edb8da82327680e3bd195ee75e216e64a4b
AvaPodrazhansky <avapod@gmail.com>
2020-03-29 18:32:16 -0400

    Added icons

--- components/cards/ProtectionMethods.js
+++ components/cards/ProtectionMethods.js
@@ -1,14 +1,24 @@
 import * as React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {Card, ListItem} from 'react-native-elements';
 import {
     PROTECTION_METHODS_TITLE,
     protectionMethods
 } from "../../constants/constant-list";
 import PropTypes from "prop-types";
-import {Entypo} from '@expo/vector-icons';
+import {Entypo, FontAwesome, Ionicons, MaterialCommunityIcons} from '@expo/vector-icons';
 
 export default function ProtectionMethods() {
+
+    const iconArray = [
+        <FontAwesome name={'home'} size={25}/>,
+        <Ionicons name={'md-people'} size={25}/>,
+        <Ionicons name={'md-hand'} size={25}/>, // possible 'ios-person
+        <MaterialCommunityIcons name={'face'} size={25}/>,
+        <Ionicons name={'ios-trash'} size={25}/>,
+        < MaterialCommunityIcons name={'medical-bag'} size={25}/>,
+        <Ionicons name={'ios-nuclear'} size={25}/>
+    ];
 
     return (
         <Card title={PROTECTION_METHODS_TITLE} titleStyle={styles.title}>
@@ -18,7 +28,8 @@ export default function ProtectionMethods() {
                         <ListItem
                             key={'protection' + i}
                             title={item}
-                            leftIcon={<Entypo name={'dot-single'}/>}
+                            // leftIcon={<Entypo name={'dot-single'}/>}
+                            leftIcon={iconArray[i]}
                             style={styles.listText}
                         />
                     );

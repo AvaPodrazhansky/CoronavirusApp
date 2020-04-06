ef0f3205592bca730554205d52a4e7adabd2e92a
AvaPodrazhansky <avapod@gmail.com>
2020-04-06 13:05:19 -0400

    Updated Terms and Conditions card to have valid props

--- components/cards/TermsAndConditions.js
+++ components/cards/TermsAndConditions.js
@@ -3,27 +3,30 @@ import {Card, ListItem} from 'react-native-elements';
 import {TERMS_TITLE, termsArray, termsTitleArray} from "../../constants/constant-list";
 import styles from './styles';
 import AcceptTermsButton from "../button/survey-question-buttons/accept-terms-button";
+import {View} from "react-native";
 
 export default function TermsAndConditions() {
 
     return (
         <Card title={TERMS_TITLE} titleStyle={styles.title} containerStyle={styles.termsAndConditionsContainer}>
-            {
-                termsArray.map((item, i) => {
-                    return (
-                        <ListItem
-                            key={'terms' + i}
-                            title={termsTitleArray[i]}
-                            titleStyle={styles.termsAndConditionsTitleText}
-                            subtitle={item}
-                            subtitleStyle={styles.termsAndConditionsSubtitleText}
-                            style={styles.listText}
-                            containerStyle={styles.termsAndConditionsItemContainer}
-                        />
-                    );
-                })
-            }
-            <AcceptTermsButton/>
+            <View>
+                {
+                    termsArray.map((item, i) => {
+                        return (
+                            <ListItem
+                                key={'terms' + i}
+                                title={termsTitleArray[i]}
+                                titleStyle={styles.termsAndConditionsTitleText}
+                                subtitle={item}
+                                subtitleStyle={styles.termsAndConditionsSubtitleText}
+                                style={styles.listText}
+                                containerStyle={styles.termsAndConditionsItemContainer}
+                            />
+                        );
+                    })
+                }
+                <AcceptTermsButton/>
+            </View>
         </Card>
     )
 };

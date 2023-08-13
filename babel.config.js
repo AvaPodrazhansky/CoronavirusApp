b816fc53a3d82998c74d6eecbad07c0025ab5799
jbhatt31 <jay.bhatt98@gmail.com>
2023-08-12 21:07:02 -0400

    Tweaked and documented babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+/**
+ * This file configures Babel. Babel is a JavaScript compiler that converts
+ * ECMAScript 2015+ code into a backwards compatible version of JavaScript in
+ * current and older browsers or environments.
+ */
+
+module.exports = (api) => {
+    // Optimizes the build process performance by caching the config function
+    // execution result
+    api.cache(true);
+
+    return {
+        // Allows Babel to transform React Native code in Expo projects
+        presets: ["babel-preset-expo"],
+    };
 };

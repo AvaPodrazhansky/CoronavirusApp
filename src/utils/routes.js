830ab654fafc7ca50062eb6f2041a524200b3110
jbhatt31 <jay.bhatt98@gmail.com>
2023-10-10 22:40:25 -0400

    Added the clinics route to the ROUTE object in routes.js

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -8,9 +8,10 @@ import { APP_NEWS_API_KEY } from "@env";
 /**
  * A JSON object that maps a specific route category to its corresponding URL.
  *
- * @type {{news: string}}
+ * @type {{clinics: string, news: string}}
  */
 const ROUTES = {
+    clinics: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?",
     news:
         "https://newsapi.org/v2/top-headlines?country=us&category=health" +
         `&q=covid-19&apiKey=${APP_NEWS_API_KEY}`,
